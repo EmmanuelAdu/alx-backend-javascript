8220a62d50ff4b9f8abe7e1d4c9e6c75c85e52d6
EmmanuelAdu <christorenzo20@gmail.com>
2024-04-11 13:30:47 +0000

    Creating Full Price for an amount

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,57 @@
+import Currency from './3-currency';
+/**
+ * Represents the pricing for the currency
+ */
+export default class Pricing {
+  /**
+   * Returns the amount of a currency
+   * @param {*Number} amount - the amount of currency
+   * @param {*Currency} currency
+   */
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  /**
+   *
+   * @param {*Number} amount - the amount of currency
+   * @param {*} conversionRate - the rate of conversion
+   * @returns - The converted price
+   */
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}

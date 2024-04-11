3d647101493df1ccd87a9446715c431f6017eed0
EmmanuelAdu <christorenzo20@gmail.com>
2024-04-11 10:17:08 +0000

    Creating main ClassRoom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,14 @@
+/**
+ * Represents a class room.
+ */
+
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}
+   * @param {Number} maxStudentsSize - the max number of students
+   */
+
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}

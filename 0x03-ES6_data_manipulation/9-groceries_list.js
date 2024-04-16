ff3e55657fe4d5c654fa11aa59ade297ee5713fe
EmmanuelAdu <christorenzo20@gmail.com>
2024-04-16 16:43:50 +0000

    New Map of items created

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,14 @@
-export default function 
+/**
+ * Retrieves a map of groceries names and their quantity
+ */
+
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}
